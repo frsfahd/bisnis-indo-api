22cdee508102c4cb704254fc8b135931fbe268c2
farras <farraselfahd2000@gmail.com>
2022-10-14 13:11:10 +0700

    add ssl config in connection

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -10,6 +10,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 // const connectionString = process.env.DATABASE_URL;
 // const pool = new Pool({
