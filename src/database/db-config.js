05b999ba6af535892b577e3e7b409ee371a7470c
farras <farraselfahd2000@gmail.com>
2022-10-14 12:55:03 +0700

    change connection method

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -4,12 +4,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = postgresql;
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+// });
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    connectionString,
 });
 
 export { pool };
